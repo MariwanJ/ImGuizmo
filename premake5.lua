d7dcafbe91bb35cfb7636f000c1581f66027786f
Mariwan <mariwan.jalal@gmail.com>
2023-02-23 21:00:44 +0100

    add premake5.lua and put source under src folder

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "imGuizmo"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/*.h",
+        "src/*.c",
+        "src/*.cpp",
+}
+
+    includedirs
+    {
+        "../../vendor/glfw/include"
+        "../../vendor/Glad/include"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
